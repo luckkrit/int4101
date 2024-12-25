99fff1cc25021fa469900ea18801e5be3d0848ef
Krit Chomaitong <luckkrit@hotmail.com>
2024-12-25 16:33:08 +0700

    add lab1, lab2, lab3 and lab4

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,24 +4,34 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://luckkrit.github.io',
+	base: '/int4101',
 	integrations: [
 		starlight({
-			title: 'My Docs',
+			title: 'INT4101',
 			social: {
 				github: 'https://github.com/withastro/starlight',
 			},
 			sidebar: [
+				// {
+				// 	label: 'Guides',
+				// 	items: [
+				// 		// Each item here is one entry in the navigation menu.
+				// 		{ label: 'Example Guide', slug: 'guides/example' },
+				// 	],
+				// },
+				// {
+				// 	label: 'Reference',
+				// 	autogenerate: { directory: 'reference' },
+				// },
 				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
+					label: 'Labs',
+					autogenerate: { directory: 'labs' },
 				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
+			],
+			customCss: [
+				// Relative path to your custom CSS file
+				'./src/styles/kbd.css',
 			],
 		}),
 	],
